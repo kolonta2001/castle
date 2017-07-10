0089742bc50864022e4bef8d9480da4c080ce4fa
Lihong.Gu <529434537@qq.com>
2017-07-10 21:41:20 +0800

    init java test module

--- Java/src/ThreadLocalTest.java
+++ Java/src/ThreadLocalTest.java
@@ -2,6 +2,8 @@
  * Created by corcor on 2017/7/10.
  */
 public class ThreadLocalTest {
-    
 
+    public static void main(String[] args) {
+        ThreadLocal threadLocal = new ThreadLocal();
+    }
 }
